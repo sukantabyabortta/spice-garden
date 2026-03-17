63fe4cc0b6ecd2c83c7c584cb382ca53e24098ee
SukantaWeb <sukanta.byabortta@gmail.com>
2026-03-17 13:41:49 +0530

    Initial React Project Upload

--- src/index.js
+++ src/index.js
@@ -9,10 +9,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <BrowserRouter basename="/spicegarden">
+  //     <App />
+  //   </BrowserRouter>
+  // </React.StrictMode>
+  <BrowserRouter basename="/spicegarden">
+    <App />
+  </BrowserRouter>
 );
 reportWebVitals();
