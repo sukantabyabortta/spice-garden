2d03dc5e14519d05b933cc25b48804c7f00ca161
SukantaWeb <sukanta.byabortta@gmail.com>
2026-03-19 02:59:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,8 +14,8 @@ root.render(
   //     <App />
   //   </BrowserRouter>
   // </React.StrictMode>
-  <BrowserRouter basename="/spice-garden">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 reportWebVitals();
