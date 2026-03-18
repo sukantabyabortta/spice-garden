7d0fdd229d4799c2d9bcffffb0c9b5d2792e7c0a
SukantaWeb <sukanta.byabortta@gmail.com>
2026-03-19 02:22:30 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <Offer />
 
       <Routes>
-        <Route path="/spice-garden" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/menu" element={<Menu />} />
 
